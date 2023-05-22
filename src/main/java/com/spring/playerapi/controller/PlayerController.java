9d8b83497a369d8d535e7f40a486fbf3c6f37e70
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-23 00:46:41 +0300

    Create PlayerController

---
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -0,0 +1,15 @@
+package com.spring.playerapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//localhost8080:
+@RestController
+@RequestMapping("/api")
+//localhost:8080/api/
+public class PlayerController {
+	
+	
+
+}
