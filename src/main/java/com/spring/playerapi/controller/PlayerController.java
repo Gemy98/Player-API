3c8274e525cd8094a3e8ae140c77fb6ae735626e
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-23 03:52:40 +0300

    Update project dependencies and Configuraion class

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -1,15 +1,23 @@
 package com.spring.playerapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//localhost8080:
+//localhost8080:spring-crm-rest
 @RestController
 @RequestMapping("/api")
-//localhost:8080/api/
+//localhost:8080/spring-crm-rest/api/
 public class PlayerController {
 	
+	
+	//localhost:8080/spring-crm-rest/api/data
+	@GetMapping("/data")
+	public String getData()	{
+		
+		return"mydata";
+	}
 	
 
 }
